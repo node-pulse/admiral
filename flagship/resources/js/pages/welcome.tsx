f8af7eea93aec43e1f94070d038479e3486a9bd5
Yumin Gui <guiyumin@protonmail.com>
2025-11-20 21:12:16 -0800

    fix: improve server ID generation logic and file permission handling in deployment playbook

--- flagship/resources/js/pages/welcome.tsx
+++ flagship/resources/js/pages/welcome.tsx
@@ -65,7 +65,7 @@ export default function Welcome({
                 </header>
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
-                        <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
+                        <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-5 shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
                             <h1 className="mb-1 font-medium">
                                 {translations.app_title}
                             </h1>
@@ -109,7 +109,12 @@ export default function Welcome({
                                             target="_blank"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
-                                            <span>{translations.content.documentation}</span>
+                                            <span>
+                                                {
+                                                    translations.content
+                                                        .documentation
+                                                }
+                                            </span>
                                             <svg
                                                 width={10}
                                                 height={11}
@@ -140,7 +145,9 @@ export default function Welcome({
                                             target="_blank"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
-                                            <span>{translations.content.youtube}</span>
+                                            <span>
+                                                {translations.content.youtube}
+                                            </span>
                                             <svg
                                                 width={10}
                                                 height={11}
@@ -171,7 +178,7 @@ export default function Welcome({
                                 </li>
                             </ul>
                         </div>
-                        <div className="relative -mb-px flex aspect-[335/376] w-full shrink-0 items-center justify-center overflow-hidden rounded-t-lg bg-white p-8 lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]">
+                        <div className="relative -mb-px flex aspect-335/376 w-full shrink-0 items-center justify-center overflow-hidden rounded-t-lg bg-white p-8 lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]">
                             <img
                                 src="/nodepulse.jpg"
                                 alt="Node Pulse Logo"
