a8302c0966d57f8c0b29d775866a8f785cb5fddf
Yumin Gui <guiyumin@protonmail.com>
2025-11-22 21:56:52 -0800

    fix: improve formatting of file type description in VariableFileUploader button

--- flagship/resources/js/components/deployments/create/variable-file-uploader.tsx
+++ flagship/resources/js/components/deployments/create/variable-file-uploader.tsx
@@ -165,7 +165,7 @@ export function VariableFileUploader({
                 onClick={handleFileClick}
             >
                 <Upload className="mr-2 h-4 w-4" />
-                Upload Variables (.env,.json,.yml,.yaml)
+                Upload Variables (.env, .json, .yml, .yaml)
             </Button>
         </>
     );
